bb38c513ca649069f4be2d6d70cc41c7dc1b97b4
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2023-09-25 19:05:44 -0300

    feat: implementação inicial de ciclos do pomodoro.

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent {
+
+  shortBreakNumber: number = 0;
+  currentStage: string = 'Pomodoro'; // Pode ser 'Pomodoro', 'Short Break', ou 'Long Break'
+  timer: number = 1500; // Tempo em segundos (25 minutos para Pomodoro)
+  timerRunning: boolean = false;
+
   isConfigurationsOpen: boolean = false;
   successMessage: boolean = false;
 
@@ -20,5 +26,68 @@ export class TimerComponent {
     setTimeout(() => {
       this.successMessage = false;
     }, 1500);
+  }
+
+  startTimer() {
+    this.timerRunning = true;
+    const intervalId = setInterval(() => {
+      if(!this.timerRunning){
+        clearInterval(intervalId);
+        return;
+      }
+      if (this.timer > 0) {
+        this.timer--;
+      } else {
+        this.timerRunning = false;
+        clearInterval(intervalId);
+        this.switchToNextStage(); // Chame a função para alternar para a próxima fase
+      }
+    }, 1000);
+  }
+
+
+
+  pauseTimer() {
+    //isso não funciona
+    this.timerRunning = false;
+  }
+
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const minutesStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const secondsStr = remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+    return `${minutesStr}:${secondsStr}`;
+  }
+
+  switchToNextStage() {
+    //precisa validar se é inicio automatico.
+    //ou vai criar uma mensagem para o usuário clicar e ir para o proximo stage.
+    //o timer deve vir do que foi setado no settings
+    let newStage = 'Pomodoro';
+    let newTimer = 1500;
+    if(this.currentStage == 'Pomodoro'){
+
+      if(this.shortBreakNumber == 4) {
+        newStage = 'Long Break';
+        newTimer = 900; // (15 minutos)
+        this.shortBreakNumber = 0;
+      }
+      else{
+        newStage = 'Short Break';
+        newTimer = 300; // Tempo para a pausa curta em segundos (5 minutos)
+        this.shortBreakNumber++;
+      }
+    }
+
+    this.switchStage(newStage, newTimer, this.shortBreakNumber, true);
+  }
+
+
+  switchStage(newStage: string, newTimer: number, newShortBreakNumber: number, next: boolean = false) {
+    this.currentStage = newStage;
+    this.timer = newTimer;
+    this.shortBreakNumber = newShortBreakNumber;
+    this.timerRunning = next;
   }
 }
