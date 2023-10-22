4b0f20faa985cd5772e9179a0436b3427c708dc6
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2023-10-22 20:44:36 -0300

    feat: implementação de feedback sonoro ao término de etapa do ciclo de pomodoro.

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,13 +1,26 @@
-import { Component, booleanAttribute, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  booleanAttribute,
+  ChangeDetectorRef,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { TimerSettings } from '../settings/settings.interface';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+  styleUrls: ['./timer.component.css'],
 })
 export class TimerComponent {
-  pomodoroSettings: TimerSettings = {pomodoroDuration: 1500, shortBreakDuration: 300, longBreakDuration: 900, autoStart: false} as TimerSettings;
+  @ViewChild('audio', { static: true }) audioElement!: ElementRef;
+
+  pomodoroSettings: TimerSettings = {
+    pomodoroDuration: 1500,
+    shortBreakDuration: 300,
+    longBreakDuration: 900,
+    autoStart: false,
+  } as TimerSettings;
   shortBreakNumber: number = 0;
   currentStage: string = 'Pomodoro';
   timer: number = 1500;
@@ -18,13 +31,13 @@ export class TimerComponent {
 
   timerInterval: any;
 
-  constructor(private changeDetectorRef: ChangeDetectorRef){}
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   toggleConfigurations() {
     this.isConfigurationsOpen = !this.isConfigurationsOpen;
   }
 
-  onConfigSaved($event : TimerSettings) {
+  onConfigSaved($event: TimerSettings) {
     this.isConfigurationsOpen = false;
     this.successMessage = true;
 
@@ -43,7 +56,7 @@ export class TimerComponent {
 
     this.timer = this.pomodoroSettings.pomodoroDuration;
     this.currentStage = 'Pomodoro';
-    if(!this.pomodoroSettings.autoStart) return;
+    if (!this.pomodoroSettings.autoStart) return;
     setTimeout(() => {
       this.startTimer();
     }, 1000);
@@ -73,35 +86,54 @@ export class TimerComponent {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
     const minutesStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const secondsStr = remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+    const secondsStr =
+      remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
     return `${minutesStr}:${secondsStr}`;
   }
 
   switchToNextStage() {
     let newStage = 'Pomodoro';
     let newTimer = this.pomodoroSettings.pomodoroDuration;
-    if(this.currentStage == 'Pomodoro'){
-
-      if(this.shortBreakNumber == 4) {
+    if (this.currentStage == 'Pomodoro') {
+      if (this.shortBreakNumber == 4) {
         newStage = 'Long Break';
         newTimer = this.pomodoroSettings.longBreakDuration;
         this.shortBreakNumber = 0;
-      }
-      else{
+      } else {
         newStage = 'Short Break';
         newTimer = this.pomodoroSettings.shortBreakDuration;
         this.shortBreakNumber++;
       }
     }
 
-    this.switchStage(newStage, newTimer, this.shortBreakNumber, this.pomodoroSettings.autoStart);
+    this.switchStage(
+      newStage,
+      newTimer,
+      this.shortBreakNumber,
+      this.pomodoroSettings.autoStart
+    );
+    this.playSound();
   }
 
-  switchStage(newStage: string, newTimer: number, newShortBreakNumber: number, next: boolean = false) {
+  switchStage(
+    newStage: string,
+    newTimer: number,
+    newShortBreakNumber: number,
+    next: boolean = false
+  ) {
     this.currentStage = newStage;
     this.timer = newTimer;
     this.shortBreakNumber = newShortBreakNumber;
     this.timerRunning = next;
-    if(this.timerRunning) this.startTimer()
+    if (this.timerRunning) this.startTimer();
+  }
+
+  playSound() {
+    console.log('this.audioElement');
+    if (this.audioElement && this.audioElement.nativeElement) {
+      this.audioElement.nativeElement.volume = 1.0;
+
+      this.audioElement.nativeElement.play();
+    }
   }
 }
