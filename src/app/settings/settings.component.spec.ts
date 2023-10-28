5d3f94293f7af918f410196e91cd83ac33b41df1
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2023-10-28 20:44:32 -0300

    test: foram adicionados testes para os components

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,21 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SettingsComponent } from './settings.component';
+import { TimerSettings } from './settings.interface';
+import { Component, EventEmitter } from '@angular/core';
+
+// Stub para simular o EventEmitter
+class EventEmitterStub<T> extends EventEmitter<T> {
+  constructor() {
+    super();
+  }
+}
+
+@Component({
+  selector: 'app-stub',
+  template: ''
+})
+class StubComponent {}
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [SettingsComponent]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SettingsComponent, StubComponent],
+    }).compileComponents();
+
+    TestBed.overrideComponent(SettingsComponent, {
+      set: {
+        providers: [{ provide: EventEmitter, useClass: EventEmitterStub }]
+      }
     });
+
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit configSaved event with correct settings', async () => {
+    const emittedSettings: TimerSettings = {
+      pomodoroDuration: 25 * 60,
+      shortBreakDuration: 5 * 60,
+      longBreakDuration: 15 * 60,
+      autoStart: false
+    };
+
+    component.tempPomodoroDuration = 25;
+    component.tempShortBreakDuration = 5;
+    component.tempLongBreakDuration = 15;
+    component.tempAutoStart = false;
+
+    let emitted: TimerSettings | undefined;
+    (component.configSaved as EventEmitter<TimerSettings>).subscribe((settings) => {
+      emitted = settings;
+    });
+
+    component.onSaveSettings();
+
+    await fixture.whenStable();
+
+    expect(emitted).toEqual(emittedSettings);
   });
 });
--- src/app/timer/timer.component.spec.ts
+++ src/app/timer/timer.component.spec.ts
@@ -1,21 +1,41 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TimerComponent } from './timer.component';
+
+@Component({
+  selector: 'app-settings',
+  template: ''
+})
+class SettingsStubComponent {}
 
 describe('TimerComponent', () => {
   let component: TimerComponent;
   let fixture: ComponentFixture<TimerComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TimerComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        TimerComponent,
+        SettingsStubComponent
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TimerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start the timer when startTimer is called', () => {
+    component.startTimer();
+    expect(component.timerRunning).toBe(true);
+  });
+
+  it('should pause the timer when pauseTimer is called', () => {
+    component.startTimer();
+    component.pauseTimer();
+    expect(component.timerRunning).toBe(false);
   });
 });
